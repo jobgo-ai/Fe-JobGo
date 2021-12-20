0a78c447b74b97eb2fd805fa2e9e87d255685330
nick-barth <barth.nicholas@gmail.com>
2021-12-20 14:37:33 +0100

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/prettier",
+    "plugin:cypress/recommended",
+  ],
+  parserOptions: {
+    parser: "babel-eslint",
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/no-use-v-if-with-v-for": "off",
+  },
+};
