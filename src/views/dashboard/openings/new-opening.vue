c6ca386e9aea5375aef6110a879109dfc42d125b
nick-barth <barth.nicholas@gmail.com>
2021-12-28 14:57:31 +0100

    added new opening

--- src/views/dashboard/openings/new-opening.vue
+++ src/views/dashboard/openings/new-opening.vue
@@ -1,7 +1,12 @@
 <template>
   <div>new Opening</div>
+  <hp-input></hp-input>
+  <hp-button label="Click me add candidate"></hp-button>
 </template>
 
-<script setup></script>
+<script setup>
+import HpInput from "@/components/hp-input.vue";
+import HpButton from "@/components/hp-button.vue";
+</script>
 
 <style lang="scss" scoped></style>
