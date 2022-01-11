d9e302cf6b5f454ae1a24335ecd311a06c80d878
nick-barth <barth.nicholas@gmail.com>
2022-01-11 14:45:39 +0100

    added popper

--- src/components/hp-header.vue
+++ src/components/hp-header.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="header">
     <div>Hireproof</div>
+    <hp-popper>
+      <Button>Demo</Button>
+      <template #content>
+        <div>This is the Popper content 🍿</div>
+      </template>
+    </hp-popper>
     <router-link to="/settings">Nick Barth</router-link>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import HpPopper from "@/components/hp-popper.vue";
+</script>
 
 <style lang="scss" scoped>
 .header {
---
+++ src/components/hp-popper.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- Hardcoded "hover", "openDelay" and "closeDelay" -->
+  <Popper v-bind="$attrs">
+    <slot />
+    <template #content="props">
+      <slot name="content" v-bind="props" />
+    </template>
+  </Popper>
+</template>
+
+<script setup>
+import Popper from "vue3-popper";
+</script>
