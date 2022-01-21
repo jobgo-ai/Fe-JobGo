df8efbc589380205071c8aa2b4f2f18f03fc388c
nick-barth <barth.nicholas@gmail.com>
2022-01-21 14:28:29 +0100

    ?

--- src/components/hp-button.vue
+++ src/components/hp-button.vue
@@ -14,12 +14,17 @@
         :size="14"
       ></hp-spinner>
     </button>
-    <button class="hp-button__button" v-if="dropdown">Hello</button>
+    <button :class="addonButton" v-if="dropdown">
+      <hp-icon :name="dropdown" size="14" />
+    </button>
   </div>
 </template>
 
 <script setup>
+//Vendor
 import { computed } from "vue";
+// Components
+import HpIcon from "@/components/hp-icon.vue";
 import HpSpinner from "@/components/hp-spinner.vue";
 const props = defineProps({
   label: {
@@ -43,8 +48,8 @@ const props = defineProps({
     default: false,
   },
   dropdown: {
-    type: Boolean,
-    default: false,
+    type: String,
+    default: "chevron-down",
   },
 });
 
@@ -60,6 +65,14 @@ const buttonClasses = computed(() => {
   return {
     "hp-button__button": true,
     "hp-button__button--disabled": props.isDisabled,
+    "hp-button__button--dropdown": props.dropdown,
+  };
+});
+
+const addonButton = computed(() => {
+  return {
+    "hp-button__button": true,
+    "hp-button__button--addon": props.dropdown,
   };
 });
 
@@ -77,7 +90,7 @@ const emit = defineEmits(["handleClick"]);
     cursor: pointer;
     padding: 6px 16px;
     font-size: 14px;
-    background-color: var(--color-white);
+    background-color: var(--color-background);
     color: var(--color-text-primary);
     outline: none;
     border: 1px solid rgba(227, 229, 229, 1);
@@ -95,9 +108,20 @@ const emit = defineEmits(["handleClick"]);
       cursor: not-allowed;
     }
 
+    &--dropdown {
+      border-top-right-radius: 0px;
+      border-bottom-right-radius: 0px;
+    }
+
+    &--addon {
+      border-top-left-radius: 0px;
+      border-bottom-left-radius: 0px;
+      border-left: none;
+    }
+
     &:active:not([disabled]),
     &:hover:not([disabled]) {
-      background-color: var(--color-gray-background);
+      background-color: var(--color-panel);
       box-shadow: 0px 4px 8px rgba(33, 44, 51, 0.02),
         0px 0px 1px rgba(33, 44, 51, 0.02);
     }
@@ -106,7 +130,7 @@ const emit = defineEmits(["handleClick"]);
       border-color: var(--color-border-subtle);
     }
     &:focus:not([disabled]) {
-      outline: #f2f4f5 solid 4px;
+      outline: var(--color-focus) solid 4px;
       border-color: var(--color-border);
       filter: drop-shadow(0px 4px 8px rgba(33, 44, 51, 0.02))
         drop-shadow(0px 0px 1px rgba(33, 44, 51, 0.02));
