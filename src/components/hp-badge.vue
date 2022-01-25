55a69314438c6db062d26286e67dc3e92ba4b1a6
nick-barth <barth.nicholas@gmail.com>
2022-01-25 10:47:48 +0100

    added badges

---
+++ src/components/hp-badge.vue
@@ -0,0 +1,71 @@
+<template>
+  <div :class="`hp-badge hp-badge--${props.type}`">
+    <hp-icon
+      v-if="icon"
+      :size="14"
+      class="hp-badge__icon"
+      :name="icon"
+    ></hp-icon>
+    {{ props.content }}
+  </div>
+</template>
+
+<script setup>
+import HpIcon from "@/components/hp-icon.vue";
+const props = defineProps({
+  icon: {
+    type: String,
+    default: "",
+  },
+  content: {
+    type: String,
+    default: "",
+  },
+  type: {
+    type: String,
+    default: "",
+  },
+});
+</script>
+
+<style lang="scss">
+.hp-badge {
+  display: inline-flex;
+  background-color: var(--color-panel);
+  color: var(--color-text-secondary);
+  border: 1px solid var(--color-border-subtle);
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: 500;
+  padding: 2px 6px;
+  font-size: 12px;
+  line-height: 16px;
+  border-radius: 6px;
+  height: 20px;
+  &__icon {
+    margin-right: 6px;
+  }
+  &:hover {
+    background-color: var(--color-border-subtle);
+  }
+
+  &--positive {
+    background-color: var(--color-background-positive);
+    color: var(--color-forground-positive);
+    border: 1px solid var(--color-border-positive);
+    &:hover {
+      background-color: var(--color-background-positive);
+    }
+  }
+
+  &--negative {
+    background-color: var(--color-background-negative);
+    color: var(--color-forground-negative);
+    border: 1px solid var(--color-border-negative);
+    &:hover {
+      background-color: var(--color-background-negative);
+    }
+  }
+}
+</style>
---
+++ src/stories/hp-badge.stories.js
@@ -0,0 +1,25 @@
+import HpBadge from "../components/hp-badge.vue";
+
+export default {
+  title: "Components/Badge",
+  component: HpBadge,
+  argTypes: {},
+};
+
+const Template = (args) => ({
+  components: { HpBadge },
+  setup() {
+    return { args };
+  },
+  template: `<div style="display:flex">
+    <hp-badge style="margin-right:8px;" content="4.0" v-bind="args"> </hp-badge>
+    <hp-badge style="margin-right:8px;"  icon="layers" content="4" v-bind="args"> </hp-badge>
+    <hp-badge style="margin-right:8px;" content="4.0"  type="positive" v-bind="args"> </hp-badge>
+    <hp-badge style="margin-right:8px;"  icon="layers"  type="positive" content="4" v-bind="args"> </hp-badge>
+    <hp-badge style="margin-right:8px;" content="4.0"  type="negative" v-bind="args"> </hp-badge>
+    <hp-badge style="margin-right:8px;"  icon="layers"  type="negative" content="4" v-bind="args"> </hp-badge>
+  </div>`,
+});
+
+export const Primary = Template.bind({});
+Primary.args = {};
