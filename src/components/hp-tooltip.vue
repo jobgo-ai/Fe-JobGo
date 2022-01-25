f37deded36b971db73c94ab513e3e80f4e11802b
nick-barth <barth.nicholas@gmail.com>
2022-01-25 21:47:57 +0100

    finished tooltip

--- src/components/hp-tooltip.vue
+++ src/components/hp-tooltip.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="hp-tooltip__container">
-    <div @mouseleave="isHovered = false" @mouseenter="isHovered = true">
+    <div @mouseleave="handleHover" @mouseenter="handleHover">
       <slot />
     </div>
-    <div v-if="isHovered" class="hp-tooltip">
-      <div class="hp__tooltip-arrow"></div>
-      <div v-if="isHovered" class="hp-tooltip__content">
-        <slot name="content" />
+    <transition name="hp-tooltip-transition">
+      <div v-if="isHovered" class="hp-tooltip">
+        <div class="hp__tooltip-arrow"></div>
+        <div v-if="isHovered" class="hp-tooltip__content">
+          <slot name="content" />
+        </div>
       </div>
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -16,6 +18,14 @@
 import { ref } from "vue";
 
 const isHovered = ref(false);
+
+let timeout = null;
+const handleHover = () => {
+  clearTimeout(timeout);
+  timeout = setTimeout(() => {
+    isHovered.value = !isHovered.value;
+  }, 350);
+};
 </script>
 
 <style lang="scss">
@@ -37,6 +47,7 @@ const isHovered = ref(false);
   &__container {
     position: relative;
     display: inline-flex;
+    cursor: pointer;
   }
   &__content {
     white-space: nowrap;
@@ -55,5 +66,18 @@ const isHovered = ref(false);
   clip-path: polygon(0% 0%, 100% 100%, 0% 100%);
   transform: rotate(315deg);
   border-radius: 0 0 0 4px;
+}
+
+.hp-tooltip-transition {
+  transform: translateX(-50%);
+}
+.hp-tooltip-transition-enter-active,
+.hp-tooltip-transition-leave-active {
+  transition: all 0.15s cubic-bezier(0.17, 0.67, 0.83, 0.67);
+}
+.hp-tooltip-transition-enter-from,
+.hp-tooltip-transition-leave-to {
+  opacity: 0;
+  transform: translateX(-50%) translateY(-10px);
 }
 </style>
