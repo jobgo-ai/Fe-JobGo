a76b3535140e150b4c89bc2e17a615173182f57f
nick-barth <barth.nicholas@gmail.com>
2022-02-24 20:12:56 +0100

    checking storybook

--- src/stories/hp-dropdown.stories.js
+++ src/stories/hp-dropdown.stories.js
@@ -2,6 +2,8 @@ import HpDropdown from "../components/hp-dropdown.vue";
 import HpFlyoutMenu from "../components/hp-flyout-menu.vue";
 import vueRouter from "storybook-vue3-router";
 
+//console.log
+
 export default {
   title: "Components/Dropdown",
   component: HpDropdown,
