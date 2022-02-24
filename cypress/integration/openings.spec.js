7f9d54fe8b91e4a02561966a7de6e3a501e38c8c
nick-barth <barth.nicholas@gmail.com>
2022-02-24 14:31:00 +0100

    moving along with tests

--- cypress/integration/openings.spec.js
+++ cypress/integration/openings.spec.js
@@ -17,15 +17,28 @@ describe("openings", () => {
     cy.contains("Save changes").click();
   });
 
-  it("Can add an interview", () => {
-    cy.contains("TEST_OPENING").click();
-    cy.contains("Add interview").click();
-    cy.contains("TEST_INTERVIEW").
-  });
-
   it("Can archive an opening", () => {
     cy.contains("TEST_OPENING").click();
     cy.contains("Edit opening").click();
     cy.contains("Archive opening").click();
+  });
+
+  it("Can add an interview", () => {
+    cy.contains("PERMANENT_TEST_OPENING").click();
+    cy.contains("Edit opening").click();
+    cy.contains("Add interview").click();
+    cy.contains("TEST_INTERVIEW")
+      .parents(".hp-add-opening-card")
+      .find("button")
+      .contains("Add to opening")
+      .click();
+  });
+
+  it("Can remove interview from opening", () => {
+    cy.contains("PERMANENT_TEST_OPENING").click();
+    cy.contains("Edit opening").click();
+    cy.contains("TEST_INTERVIEW")
+      .find(".hp-button__button__icon--danger")
+      .click();
   });
 });
