39db949671a7ea2cf3d5652655f99b54a18123e8
nick-barth <barth.nicholas@gmail.com>
2022-02-26 19:09:52 +0100

    updated some stuff

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -17,13 +17,12 @@
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  on("before:browser:launch", (browser, launchOptions) => {
-    if (
-      (browser.name === "chrome" || browser.name === "edge") &&
-      browser.isHeadless
-    ) {
-      launchOptions.args.push("--disable-gpu");
-      return launchOptions;
+  on("before:browser:launch", (browser = {}, args) => {
+    if (browser.name === "chrome") {
+      // ^ make sure this is your browser name, you may
+      // be using 'canary' or 'chromium' for example, so change it to match!
+      args.push("--proxy-bypass-list=<-loopback>");
+      return args;
     }
   });
 };
