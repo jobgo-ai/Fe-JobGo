cc509534abdb54fe4746bc8689152ec3f0a3f3f7
nick-barth <barth.nicholas@gmail.com>
2022-07-14 12:44:17 +0200

    checking for pagination in search for caniddaate tests

--- cypress/integration/candidates.spec.js
+++ cypress/integration/candidates.spec.js
@@ -3,10 +3,15 @@ describe("candidates", () => {
     cy.login();
   });
 
-  it("Can view candidates", () => {
+  it.only("Can view and search for candidates", () => {
+    cy.intercept({
+      method: "GET",
+      url: "**/candidates?limit=15&offset=0&search=TEST_CANDIDATE_1_DONT_LET_ME_DIE&state=active",
+    }).as("candidateSearch");
     cy.visit("/openings");
     cy.contains("PERMANENT_OPENING").click();
     cy.get(`[name='search']`).type("TEST_CANDIDATE_1_DONT_LET_ME_DIE");
+    cy.wait("@candidateSearch");
     cy.contains("TEST_CANDIDATE_1_DONT_LET_ME_DIE").click();
     cy.contains("Current score");
   });
