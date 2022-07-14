bdcc84e18015418d823faefaa98164af76f95d1d
nick-barth <barth.nicholas@gmail.com>
2022-07-14 10:11:02 +0200

    differentiated between logged in and logged out user

--- src/components/hp-reports-header.vue
+++ src/components/hp-reports-header.vue
@@ -2,17 +2,29 @@
   <div class="hp-reports-header">
     <div class="hp-reports-header__left">
       <a
+        v-if="!isLoggedIn"
         class="hp-reports-header__logo"
         target="_blank"
         href="https://hireproof.io/"
         ><Logo></Logo
       ></a>
+      <router-link
+        v-else
+        class="hp-reports-header__logo"
+        to="/"
+        href="https://hireproof.io/"
+        ><Logo></Logo
+      ></router-link>
     </div>
   </div>
 </template>
 
 <script setup>
 import Logo from "@/assets/logo.svg";
+
+import useAuth from "@/composables/useAuth";
+const { user } = useAuth();
+const isLoggedIn = user;
 </script>
 
 <style lang="scss" scoped>
