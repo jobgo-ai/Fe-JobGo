fceeb0b2b2bdaefbcdc92104882c874ef5b6c132
nick-barth <barth.nicholas@gmail.com>
2022-09-06 16:11:36 +0200

    wow

--- src/composables/useAuth.js
+++ src/composables/useAuth.js
@@ -37,11 +37,11 @@ export const refreshToken = async () => {
 
 export default () => {
   const setUser = (payload, remember) => {
+    console.log("payload", payload);
     if (remember) {
       window.localStorage.setItem(AUTH_KEY, payload[AUTH_TOKEN]);
     }
     document.cookie = "backofficeUser=true; SameSite=None; Secure";
-    console.log(payload[AUTH_TOKEN]);
     state.token = payload[AUTH_TOKEN];
     state.error = undefined;
   };
